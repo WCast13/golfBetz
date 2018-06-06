c454269913d2c8043d3f800c4b5781f0dd95036b
William Castellano <castellkid@aol.com>
2018-06-06 16:45:07 -0400

    Collection View for main screen built and formatted

---
+++ golfBets/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  golfBets
+//
+//  Created by WilliamCastellano on 6/6/18.
+//  Copyright © 2018 WCTech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+  
+  @IBOutlet var textLabel: UILabel!
+}
--- golfBets/ViewController.swift
+++ golfBets/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+  @IBOutlet var collectionView: UICollectionView!
+  @IBOutlet var flowLayout: UICollectionViewFlowLayout!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    flowLayout.minimumLineSpacing = 1.0
+    flowLayout.minimumInteritemSpacing = 0.0
+    flowLayout.itemSize = CGSize(width: 50.0, height: 25.0)
+    
+    
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 147
   }
-
-
+  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+    cell.textLabel.text = "\(indexPath.row + 1)"
+    
+    if indexPath.row % 2 == 0 {
+      cell.backgroundColor = UIColor.green
+    } else {
+      cell.backgroundColor = UIColor.yellow
+    }
+    
+    return cell
+  }
 }
-
